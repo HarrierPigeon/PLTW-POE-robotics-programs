c25785760572ff2a20fbdc2dcf2429427dffc4f1
HarrierPigeon <32232925+HarrierPigeon@users.noreply.github.com>
2018-04-14 09:58:54 -0500

    * needed to do

--- A3_1_2_Part1.c
+++ A3_1_2_Part1.c
@@ -11,7 +11,7 @@ task main()
   while(1)
     {
     // Place control code here
-    turnLEDOff(rLED);
+    turnLEDOff(s);
     waitInMilliseconds(500);
     turnLEDOn(gLED);
     waitInMilliseconds(500);
